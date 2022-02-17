e340f44fbc96a1892030924644a1e12c84ce6ce8
LeoQiao18 <qiaofeitong@hotmail.com>
2022-02-17 00:37:55 -0500

    add more home packages

--- home/home.nix
+++ home/home.nix
@@ -1,13 +1,39 @@
 { config, pkgs, ... }:
-{
-  
-  programs.home-manager.enable = true;
-
-  home.packages = with pkgs; [
+let
+  defaultPkgs = with pkgs; [
     fd
     xsel
+    nodejs
+    yarn
     slack
+    spotify
   ];
 
+  haskellPkgs = with pkgs.haskellPackages; [
+    brittany
+    cabal2nix
+    cabal-install
+    ghc
+    haskell-language-server
+    hoogle
+    nix-tree
+  ];
+in
+{
+  programs.home-manager.enable = true;
+
   imports = (import ./programs);
+
+  home.packages = defaultPkgs ++ haskellPkgs;
+
+  programs = {
+    fzf.enable = true;
+    htop.enable = true;
+    jq.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    bat.enable = true;
+  };
 }
