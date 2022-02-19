734bda006c15d1eca218fbd3a51deafaba5ed595
LeoQiao18 <qiaofeitong@hotmail.com>
2022-02-18 23:17:11 -0500

    fix alacritty settings

--- home/programs/alacritty/default.nix
+++ home/programs/alacritty/default.nix
@@ -5,15 +5,15 @@
     enable = true;
     settings = {
       background_opacity = 0.8;
-    };
-    font = {
-      size = 16;
-    };
-    window = {
-      decorations = "full";
-      padding = {
-        x = 5;
-        y = 5;
+      font = {
+        size = 16;
+      };
+      window = {
+        decorations = "full";
+        padding = {
+          x = 5;
+          y = 5;
+        };
       };
     };
   };
